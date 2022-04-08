9b83156eaf1b3efd31b7c48a24b105aa7e9ca329
XTsat <xiaotong1238@gmail.com>
2022-04-08 21:46:41 +0800

    初版
    Steam++

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+---
+
+# 关于我
+
---
+++ docusaurus.config.js
@@ -0,0 +1,122 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '晓同',
+  tagline: '各种指南',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+    // 顶栏
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '晓同',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '指南',
+          },
+          { to: '/blog', label: '博客', position: 'left' },
+          {
+            href: 'https://github.com/XTsat',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+
+      // 底栏
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '指南',
+            items: [
+              {
+                label: 'Guide',
+                to: '/docs/',
+              },
+            ],
+          },
+          {
+            title: '关于我',
+            items: [
+              {
+                label: 'BiliBili 📺',
+                href: 'https://space.bilibili.com/53283118',
+              },
+              {
+                label: 'Steam 🎮',
+                href: 'https://steamcommunity.com/id/XTxiaotong',
+              },
+              {
+                label: '𝖯𝗅𝖺𝗒𝗅𝗂𝗌𝗍 🎵',
+                href: 'https://music.163.com/#/user/home?id=283588276',
+              },
+            ],
+          },
+          {
+            title: '更多',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/XTsat',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.made by 晓同`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
